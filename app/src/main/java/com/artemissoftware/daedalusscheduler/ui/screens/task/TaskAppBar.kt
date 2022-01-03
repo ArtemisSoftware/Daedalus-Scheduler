3306e98f29be37a56a3a05ac597ed09a921adbff
gustavo <gustavo.isaac@gmail.com>
2022-01-03 12:37:02 +0000

    Alert Dialog - Delete Single Task

--- app/src/main/java/com/artemissoftware/daedalusscheduler/ui/screens/task/TaskAppBar.kt
+++ app/src/main/java/com/artemissoftware/daedalusscheduler/ui/screens/task/TaskAppBar.kt
@@ -6,11 +6,12 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.artemissoftware.daedalusscheduler.R
+import com.artemissoftware.daedalusscheduler.components.DisplayAlertDialog
 import com.artemissoftware.daedalusscheduler.data.models.Priority
 import com.artemissoftware.daedalusscheduler.data.models.ToDoTask
 import com.artemissoftware.daedalusscheduler.ui.theme.topAppBarBackgroundColor
@@ -107,10 +108,38 @@ fun ExistingTaskAppBar(
         },
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
         actions = {
-            DeleteAction(onDeleteClicked = navigateToListScreen)
-            UpdateAction(onUpdateClicked = navigateToListScreen)
+            ExistingTaskAppBarActions(
+                selectedTask = selectedTask,
+                navigateToListScreen = navigateToListScreen
+            )
         }
     )
+}
+
+
+@Composable
+fun ExistingTaskAppBarActions(
+    selectedTask: ToDoTask,
+    navigateToListScreen: (Action) -> Unit
+) {
+    var openDialog by remember { mutableStateOf(false) }
+
+    DisplayAlertDialog(
+        title = stringResource(
+            id = R.string.delete_task,
+            selectedTask.title
+        ),
+        message = stringResource(
+            id = R.string.delete_task_confirmation,
+            selectedTask.title
+        ),
+        openDialog = openDialog,
+        closeDialog = { openDialog = false },
+        onYesClicked = { navigateToListScreen(Action.DELETE) }
+    )
+
+    DeleteAction(onDeleteClicked = { openDialog = true })
+    UpdateAction(onUpdateClicked = navigateToListScreen)
 }
 
 @Composable
